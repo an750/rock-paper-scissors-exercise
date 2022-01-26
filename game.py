f9a35141572189ab70fad7c22f3aadc1d1539ea6
an750 <an750@georgetown.edu>
2022-01-26 18:20:19 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -1,7 +1,24 @@
-
-
-
-#
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
-#
+
+
+def RPS():
+
+# ASK FOR USER INPUT
+    print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+    user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+    print("User Chose: ", user_choice)
+
+# VALIDATIONS
+
+
+# COMPUTER CHOICE
+
+
+# DETERMINE THE WINNER
+
+
+# FINAL RESULTS
+
+RPS()
